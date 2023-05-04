d5a1e4c3b73af9ad07a700088ee3fa32ca8fd101
Ilya Nuevo <copyhold@outlook.com>
2023-05-04 08:12:22 +0300

    import

--- button.tsx
+++ button.tsx
@@ -1,4 +1,5 @@
 //** @jsxImportSource https://esm.sh/preact */
+import { h, html } from "https://deno.land/x/htm@0.0.10/mod.tsx";
 export function Button(props) {
     return <button type="button" onClick={props.onClick}>click me</button>
 }
