f7e3b59beb0d9e0abda225a779e2e8b48836722c
Ilya Nuevo <copyhold@outlook.com>
2023-05-04 08:11:20 +0300

    fix errror

--- button.tsx
+++ button.tsx
@@ -1,5 +1,4 @@
-/** @jsx h */
-
-export const Button = (props) => {
+//** @jsxImportSource https://esm.sh/preact */
+export function Button(props) {
     return <button type="button" onClick={props.onClick}>click me</button>
 }
